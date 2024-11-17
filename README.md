b1fccdd371657cdb707976b53170e90968f2b761
uwicode <142529921+uwicode@users.noreply.github.com>
2024-11-17 00:18:55 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # godsays
+
 terry davis was right all along
+
+## suggestions
+
+add to your system32 folder for use in any cmd prompt
---
+++ godsays.bat
@@ -0,0 +1,41 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyH0UHShgYzy+CV+TKpwowc3I08iGtnIpW90aYIrk+buANuUB/kD2So57gkZSmdscFQxXbBa4Uic1uX5b+FC9A6c=
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF65
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGagwXEEByNoeTCrEEKOL5Av1Mr6+9WzrXI4TOcAW4DVzLGcNOUB1V2yIaoq2X5JmdIfQh5Ae3I=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+echo [34;107m
+:GODSAYS
+powershell -NoProfile -Command ^
+$HAPPY="(Get-Content -Path Happy.TXT | Get-Random -Count 32) -join ' '"; ^
+Write-Output $HAPPY;
+echo|set /p=[0m
